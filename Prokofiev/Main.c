523c6904224a1e79bed55b24edae9ac0716e2128
prokjack <prokjack@gmail.com>
2016-11-29 11:23:25 +0200

    added myAtoi

--- Prokofiev/Main.c
+++ Prokofiev/Main.c
@@ -1,11 +1,20 @@
 //
 // Created by j2ck on 07/11/16.
 //
-#include "calc/calc.h"
-#include "sortArray99/sort.h"
+#include <stdio.h>
+#include "myAtoi/myAtoi.h"
 
 int main(void) {
 //    calcFloat();
-    sort();
+//    sort();
+//    test();
+    char *p_str;
+    char str[] = "-2147483649";
+//    char str[24] = "213 342";
+    char str2[24] = "21wer323342433";
+    p_str = str;
+    int res = myAtoi(p_str);
+    printf("%d\n", res);
+    printf("%s\n", str2);
     return 0;
 }
---
+++ Prokofiev/Test/test_loops.h
@@ -0,0 +1,10 @@
+//
+// Created by j2ck on 19/11/16.
+//
+
+#ifndef NEW_TEST_LOOPS_H
+#define NEW_TEST_LOOPS_H
+
+int test(void);
+
+#endif //NEW_TEST_LOOPS_H
